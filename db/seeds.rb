8db264d69476fa258b426958937368368173d50e
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-11-08 17:02:48 -0500

    seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+#Song.create!([
+#  {song: "Broken Just Fine-JP", tags: "Singer-Songwriter,Light,Playful,bouncy"}
+#  ])
